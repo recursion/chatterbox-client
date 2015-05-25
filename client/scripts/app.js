158cd14d16a15894b5b7f9d5f790a8645a5a0cf3
Michael Symmes <msymmes@gmail.com>
2015-05-25 18:55:37 -0400

    Implemented basic parse/cleaner

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,72 @@
-// YOUR CODE HERE:
-var app = { };
+var app = {
+
+  init: function() {},
+
+  server: 'https://api.parse.com/1/classes/chatterbox',
+
+  messages: [],
+
+  send: function(message) {
+    var app = this;
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  updateMessages: function ( ) {
+    var app = this;
+    var msgs = this.messages;
+    _.each( msgs, function (message) {
+      var user = "";
+      var msgtxt = "";
+        if ( message.username ) {
+        user = app.clean(message.username);
+        }
+        if ( message.text ) {
+          msgtxt = app.clean(message.text);
+        }
+      var msg = $('<li><span class="createdBy">'+user+'</span>: '+msgtxt+'</li>');
+      $('.messages').append(msg);
+      console.log(msg);
+    })
+
+  },
+
+  fetch: function( ) {
+    var app = this;
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        app.messages = data.results;
+        app.updateMessages( );
+        console.log('chatterbox: Messages fetched');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message');
+      }
+    });
+  },
+
+  clean: function( string ) {
+    var cleanString = string.replace(/[|&;$%@"<>()+,]/g, "");
+    return cleanString;
+  }
+
+};
+
+app.fetch();
